86b40483fcc32cb0756397ab53273c0372c1c068
Yeon Jeffrey <yeonjeseo@hotmail.com>
2023-03-23 00:02:07 +0900

    fix: implement ajax call within cron job

--- src/cron/cron.module.ts
+++ src/cron/cron.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CronService } from './cron.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   providers: [CronService],
 })
 export class CronModule {}
--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class CronService {
-  @Cron('* * * * * *')
-  test() {
-    console.log('Hello World!');
+  constructor(private readonly httpService: HttpService) {}
+  @Cron('*/2 * * * * *')
+  async test() {
+    try {
+      const { data } = await firstValueFrom(
+        this.httpService.get('https://jsonplaceholder.typicode.com/todos/1'),
+      );
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
