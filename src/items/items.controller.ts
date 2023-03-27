8e5ffd7de74c09b26fd36eec48ef1b2d2ed3aafa
Yeon Jeffrey <yeonjeseo@hotmail.com>
2023-03-28 00:41:15 +0900

    fix: implement get items api

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ItemsService } from './items.service';
 
 @Controller()
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
+
+  @Get('/items/:storeId')
+  async getAllItemsByStore(@Param() { storeId }: any) {
+    return this.itemsService.getAllItemsByStore(storeId);
+  }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,6 +1,17 @@
 import { Repository } from 'typeorm';
 import { Item } from './entities/items.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 export class ItemsService {
-  constructor(private readonly items: Repository<Item>) {}
+  constructor(
+    @InjectRepository(Item)
+    private readonly items: Repository<Item>,
+  ) {}
+
+  /**
+   * TODO
+   * storeId 를 FK로 컬럼 저장해야 함
+   * storeId로 items 테이블 조회 가능해야 함.
+   */
+  getAllItemsByStore = (storeId) => this.items.find({});
 }
